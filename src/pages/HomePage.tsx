c252dac5fff558983d87be7c970ade68d94efaa3
Tannatee Juchan <ju.tannatee2543@gmail.com>
2023-10-31 02:25:11 +0700

    feat: add style to text

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -3,7 +3,7 @@ import { Fragment, FunctionComponent } from "react";
 export const HomePage: FunctionComponent = () => {
   return (
     <Fragment>
-      <h1>HomePage</h1>
+      <h1 className="text-lg">HomePage</h1>
     </Fragment>
   );
 };
